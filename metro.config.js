e1b3fcb8e69f0052c551d83b5b736f075dee6b16
dell_zdyl <dell@zdyl>
2019-05-29 14:55:48 +0800

    init

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-less-transformer")
+      // babelTransformerPath: require.resolve('./rn-transformer.js'),
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "less"]
+    }
+  };
+})();
